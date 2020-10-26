1521c1d574bf0a607456598a39205d9113589020
marcosluduea89 <72500833+marcosluduea89@users.noreply.github.com>
2020-10-25 22:19:18 -0300

    actualizacion ejercicios

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -17,6 +17,8 @@ __version__ = "1.1"
 
 
 def ej1():
+    print("------------------------")
+    print("-----Ejercicios N°1-----")
     # Ejercicios de práctica numérica
 
     # Comparadores
@@ -28,55 +30,137 @@ def ej1():
 
     # Compare cual de los dos números es mayor
     # Imprima en pantalla según corresponda
+    print("------------------------")
 
+    print("¿Cual es el mayor de los numeros? \n ")
+   
+    if numero_1 == numero_2:
+        print ("Son iguales los numeros, ingrese valores diferentes.")
+        
+    elif numero_1 > numero_2:
+        print ("El Numero {} es mayor que el numero {} ".format(numero_1,
+                                                                numero_2))
+    else: print("El Numero {} es mayor que el numero {} ".format(numero_2,
+                                                                numero_1))
+    print("------------------------")
+    print("Verificación de Positivo , negativo o cero del primer numero ingresado ")
+    
     # Verifique si el numero_1 positivo, negativo o cero
     # Imprima el resultado en cada caso
 
-    # Verifique si el numero_1 es mayor a 0 y menor a 100
-    # Imprima en pantalla si se cumple o no la condición
+    if numero_1 > 0:
+        print("El numero {} es Positivo. ".format(numero_1))
+    elif numero_1 < 0:
+        print("El numero {} es Negativo. ".format(numero_1))
+    else:
+        print("El numero es igual a cero. ")
 
+    print("------------------------")
+    # Verifique si el numero_1 es mayor a 0 y menor a 100
+
+    print("¿El numero {} es mayor a 0 y menor a 100?".format(numero_1))
+    
+    if numero_1 > 0 and (numero_1 < 100) :
+        print("El numero {} cumple la condición. ".format(numero_1))
+    else: 
+        print("El numero {} no cumple la condición. ".format(numero_1))
+
+    print("------------------------")
+
+    # Imprima en pantalla si se cumple o no la condición
     # Verifique si el numero_1 es menor a 10 o el numero_2
     # es mayor a -2
     # Imprima en pantalla si se cumple o no la condición
 
+    print("¿El numero {} es menor a diez o el numero {} menor a -2?".format(numero_1,numero_2))
+    
+    if (numero_1 < 10) or (numero_2 < -2):
+        print("La condición se cumple.")
+    else:
+        print("La condicion no se cumple.")
+    
+    # Imprima en pantalla si se cumple o no la condición
+
 
 def ej2():
+    print("------------------------")
+    print("-----Ejercicios N°2-----")
     # Ejemplos variables de texto
 
     # Comparadores
     # Ingrese dos palabras cualesquiera y realice las sigueintes
     # comparaciones entre ellas
-    texto_1 = str(input('Ingrese la primera palabra:\n'))
 
-    texto_2 = str(input('Ingrese la segunda palabra:\n'))
+    texto_1 = str(input("Ingrese la primera palabra:"))
+
+    texto_2 = str(input("Ingrese la segunda palabra:"))
 
     # Compare cual de las dos palabras es mayor (alfabéticamente)
     # Imprima en pantalla según corresponda
+    print("------------------------")
+    
+    
+    if texto_1 > texto_2:
+        print("El texto {} es el mayor alfabeticamente".format(texto_1))
+    else:
+        print("El texto {} es el mayor alfabeticamente".format(texto_2))
 
     # Compare cual de las dos palabras tiene mayor
     # cantidad de letras
     # Imprima en pantalla según corresponda
+    leng_texto_1 = len (texto_1) 
+    leng_texto_2 = len (texto_2)
+
+    if leng_texto_1 > leng_texto_2:
+        print("La palabra {} tiene la mayor cantidad de palabras ".format(texto_1))
+    else:
+        print("La palabra {} tiene la mayor cantidad de palabras ".format(texto_2))
 
     # Verifique si la primera letra de la primera palabra
     # es mayor a la primera letra de la segunda palabra
     # Imprima en pantalla según corresponda
+    if texto_1[0] > texto_2 [0]:
+        print("La palabra {} contine la primer letra mayor a la segunda".format(texto_1))
+    else:
+        print("La palabra {} contine la primer letra mayor a la segunda".format(texto_1))
+    print("------------------------")
 
     copia_texto_1 = texto_1  # Copia de la variable texto_1
 
+    if texto_1 == copia_texto_1:
+        print("Son iguales")
+    else:
+        print("No son iguales")
+
     # Verifique que copia_texto_1 es igual a texto_1
     # Imprima en pantalla según corresponda
-
+    if copia_texto_1 != texto_1:
+        print("Son distintos")
+    else:
+        print("Son iguales")
     # Verifique que copia_texto_1 es distinta a texto_2
     # Imprima en pantalla según corresponda
 
 
 def ej3():
     # Ejercicios de práctica numérica
-
-    # Condicionales anidados
+    print("------------------------")
+    print("-----Ejercicios N°3-----")
+    print( "Condicionales anidados" ) 
     numero_1 = 7
     numero_2 = -2
 
+    if numero_1 > 5: 
+        if numero_2 > 0:
+            print ("Resp=1")
+        else:
+            print ("Resp=2")
+    else : 
+        if numero_2 > 5:
+            print ("Resp=3")
+        else:
+            print ("Resp=4")
+        
     # Verifique si el numero_1 es mayor a 5
     #   --> En caso afirmativo, verifique si el numero_2
     #       es positivo
@@ -86,34 +170,60 @@ def ej3():
     #      verifique si el numero_2 es mayor a 5
     #       --> En caso afirmativo imprima en pantalla "Resp=3"
     #       --> En caso negativo imprima en pantalla "Resp=4"
-
+    print("------------------------")
+    print("Verificar la calificación de un estudiante \n")
     # Verifique la calificación de un estudiante según su
     # puntaje en un examen
-    puntaje = 70
+    puntaje = int(input("Ingrese el puntaje:"))
+
+    if puntaje >=90:
+        print("A")
+    elif puntaje >=80:
+        print("B")
+    elif puntaje >= 70:
+        print("C")
+    elif puntaje >= 60:
+        print("D")
+    elif puntaje < 60:
+        print("F")
 
     # Si el puntaje es mayor igual a 90 --> imprimir A
     # Si el puntaje es mayor igual a 80 --> imprimir B
     # Si el puntaje es mayor igual a 70 --> imprimir C
     # Si el puntaje es mayor igual a 60 --> imprimir D
     # Si el puntaje es manor a  60      --> imprimir F
-
+    
     # Debe imprimir en pantalla la calificacion
     # Utilizar "if" anidados
-
-
 def ej4():
     # Ejemplos variables de texto
-
+    print("------------------------")
+    print("-----Ejercicios N°4-----")
     texto_1 = '5'
     texto_2 = '7'
 
     # Verifique cual cual de los dos textos es mayor alfabéticamente
     # Imprima en pantalla según corresponda
 
+    if texto_1 > texto_2:
+        print("La variable {} es mayor que {} ".format(texto_1,texto_2))
+    else:
+        print("La variable {} es mayor que {} ".format(texto_2,texto_1))
+    
+    num_texto_1 = int(texto_1)
+    num_texto_2 = int(texto_2)
+
+    if num_texto_1 > num_texto_2:
+        print("El numero {} es mayor que {}".format(num_texto_1,num_texto_2))
+    else:
+        print("El numero {} es mayor que {}".format(num_texto_2,num_texto_1))
+
     # Transforma esas variables tipo texto y almacénalas
     # en nuevas variables númericas (int)
     # Repita el proceso, ¿Cuál de las nuevas variables es mayor?
     # Imprima en pantalla según corresponda
+    
+    
 
     # Para pensar!
     # ¿Por qué cree que texto_2 es mayor a texto_1?
@@ -124,9 +234,12 @@ def ej4():
     # responder.
     # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
 
+
+
 if __name__ == '__main__':
-    print("Bienvenidos a otra clase de Inove con Python")
-    #ej1()
-    #ej2()
-    #ej3()
-    #ej4()
+    print ("Bienvenidos a otra clase de Inove con Python  ")
+    ej1()
+    ej2()
+    ej3()
+    ej4()
+    
--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -16,17 +16,33 @@ __version__ = "1.3"
 
 
 def ej1():
-    print('Ejercicios de práctica con números')
+    print("------------------------")
+    print("-----Ejercicios N°1-----")
+    print('Ejercicios de práctica con números\n')
 
     '''
     Realice un programa que solicite por consola 2 números
     Calcule la diferencia entre ellos e informe por pantalla
     si el resultado es positivo, negativo o cero.
     '''
+    numero_uno = int (input("Ingrese el primer numero: "))
+    numero_dos = int (input("Ingrese el segundo numero: "))
+    
+    resta = numero_uno - numero_dos
+
+    if resta == 0:
+        print("El resultado es 0")
+    elif resta > 0:
+        print("El resultado es positivo")
+    else:
+        print("El resultado es negativo")
+
 
 
 def ej2():
-    print('Ejercicios de práctica con números')
+    print("------------------------")
+    print("-----Ejercicios N°2-----")
+    print('Ejercicios de práctica con números\n')
 
     '''
     Realice un programa que solicite el ingreso de tres números
@@ -34,10 +50,28 @@ def ej2():
     o impar.
     Para cada caso imprimir el resultado en pantalla.
     '''
+    numero_uno = int (input("Ingrese el primer numero: "))
+    numero_dos = int (input("Ingrese el segundo numero: "))
+    numero_tres = int (input("Ingrese el tercer numero: "))
 
+    if (numero_uno %2) == 1:
+        print("{} Es Impar".format(numero_uno))
+    else:
+        print("{} Es par".format(numero_uno))
+    if (numero_dos %2) == 1:
+        print("{} Es Impar".format(numero_dos))
+    else:
+        print("{} Es par".format(numero_dos))
+    if (numero_tres %2) == 1:
+        print("{} Es Impar".format(numero_tres))
+    else:
+        print("{} Es par".format(numero_tres))
+    
 
 def ej3():
-    print('Ejercicios de práctica con números')
+    print("------------------------")
+    print("-----Ejercicios N°3-----")
+    print('Ejercicios de práctica con números\n')
 
     '''
     Realice una calculadora, se ingresará por línea de comando dos números
@@ -52,10 +86,41 @@ def ej3():
     Se debe efectuar el cálculo correcto según la operación ingresada por consola
     Imprimir en pantalla la operación realizada y el resultado
     '''
+    numero_uno = int (input("Ingrese el primer numero: "))
+    numero_dos = int (input("Ingrese el segundo numero: "))
+    print("---------------")
+    print("Perfecto, a continuación ingrese la operacion deseada:")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    print("**")
 
+    suma = numero_uno + numero_dos  
+    resta = numero_uno - numero_dos
+    multiplicacion = numero_uno * numero_dos
+    division = numero_uno / numero_dos
+    exponente = numero_uno ** numero_dos 
+
+    opcion = str(input())
+
+    if opcion == "+":
+        print("El resultado de la suma entre {} y {} es : {} ".format(numero_uno,numero_dos,suma))
+    elif opcion == "-":
+        print("El resultado de la resta entre {} y {} es : {} ".format(numero_uno,numero_dos,resta)) 
+    elif opcion == "*":
+        print("El resultado de la multiplicacion entre {} y {} es : {} ".format(numero_uno,numero_dos,multiplicacion)) 
+    elif opcion == "/":
+        print("El resultado de la division es entre {} y {} : {} ".format(numero_uno,numero_dos,division))
+    elif opcion == "**":
+        print("El resultado del exponente entre {} y {} es : {} ".format(numero_uno,numero_dos,exponente))
+    else:
+        print("Esa operacion no esta habilitada, intente nuevamente.")    
 
 def ej4():
-    print('Ejercicios de práctica con cadenas')
+    print("------------------------")
+    print("-----Ejercicios N°4-----")
+    print('Ejercicios de práctica con cadenas\n')
 
     '''
     Realice un programa que solicite por consola 3 palabras cualesquiera
@@ -69,10 +134,45 @@ def ej4():
     Si se ingresa "2" por consola se deben ordenar las 3 palabras por cantidad de letras
     e imprimir en pantalla de la mayor a la menor
     '''
+    
+    palabra_uno = str (input("Ingrese la primera palabra:"))
+    palabra_dos = str (input("Ingrese la segunda palabra:"))
+    palabra_tres = str (input("Ingrese la tercer palabra:"))
+    
+
+    print("Perfecto, ¿como quiere ordenar las palabras?.\n")
+    print("1 = Ordenar por orden alfabetico")
+    print("2 = Ordenar por cantidad de letras")
+    
+    opcion = int (input("Ingrese la opcion deseada:"))
+
+    #Opcion 1 = Guardamos las palabras en una variable "biblioteca" y usamos la funcion sorted
+    biblioteca = [palabra_uno,palabra_dos,palabra_tres]
+    orden_alfabetico = sorted(biblioteca)
+
+    #Opcion 2 = Calculamos el tamaño de cada palabra y usamos la misma funcion sorted
+    orden_len_1 = len (palabra_uno)
+    orden_len_2 = len (palabra_dos)
+    orden_len_3 = len (palabra_tres)
+    numeros = [orden_len_1,orden_len_2,orden_len_3]
+    orden_len= sorted(numeros)
+
+    if opcion == 1:
+        print (orden_alfabetico[0])
+        print (orden_alfabetico[1])
+        print (orden_alfabetico[2])
+    elif opcion ==2:
+        print (orden_len[0])
+        print (orden_len[1])
+        print (orden_len[2])
+    else: 
+        print("La opcion ingresada no es válida.")
 
 
 def ej5():
-    print('Ejercicios de práctica con números')
+    print("------------------------")
+    print("-----Ejercicios N°5-----")
+    print('Ejercicios de práctica con números \n')
 
     '''
     Realice un programa que solicite ingresar tres valores de temperatura
@@ -83,11 +183,32 @@ def ej5():
 
     En cada caso imprimir en pantalla el resultado
     '''
+    print("Ingrese tres valores de temperatura en °C")
+
+    temperatura_uno = int (input("Ingrese el primer numero: "))
+    temperatura_dos = int (input("Ingrese el segundo numero: "))
+    temperatura_tres = int (input("Ingrese el tercer numero: "))
+
+    temperaturas = [temperatura_uno,temperatura_dos,temperatura_tres]
+
+    maxima = max(temperaturas)
+    minima = min(temperaturas)
+    promedio =(temperatura_uno + temperatura_dos + temperatura_tres)/3
+    print("------------------------")
+    print ("Resultados: ")
+
+    print("La temperatura maxima es {}°C ".format(maxima))
+    print("La temperatura minima es {}°C ".format(minima))
+    print("El promedio de temperatura es {}°C".format(promedio))
+
+    print("----Fin Programa-------")
+
+
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
     ej1()
-    #ej2()
-    #ej3()
-    #ej4()
-    #ej5()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
